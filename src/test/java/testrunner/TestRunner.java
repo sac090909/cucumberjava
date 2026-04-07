cf258ba543682510b07571f9ca3d104a28f43812
Shaila Nasrin <drshaila2021@gmail.com>
2026-04-07 01:38:50 -0500

    Update Runner

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/featurefiles", glue="stepdefinations", dryRun=false, monochrome=true, plugin= {"pretty","html:target/cucumber-html-report.html", "json:target/cucumber-json-report.json"})
+@CucumberOptions(features = "src/test/java/featurefiles", glue="stepdefinations", dryRun=false, monochrome=true, plugin= {"pretty","html:target/cucumber-html-report", "json:target/cucumber-json-report.json"})
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
 	@Override
